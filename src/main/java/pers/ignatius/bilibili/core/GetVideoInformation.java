cf283ffab767cbeeb00d6cc1113a69a551495509
IgnatiusGL <ignatiusgl@foxmail.com>
2021-07-10 08:52:35 +0800

    refine : 更改了错误的函数使用

--- src/main/java/pers/ignatius/bilibili/core/GetVideoInformation.java
+++ src/main/java/pers/ignatius/bilibili/core/GetVideoInformation.java
@@ -112,7 +112,7 @@ public class GetVideoInformation {
             for (int i=0;matcher.find();i++){
                 String title = matcher.group();
                 VideoInformation videoInformation = new VideoInformation();
-                videoInformation.setTitle(("P" + (i + 1) + "-" + title.substring(8,title.length()-1)).replaceAll(" ", ""));
+                videoInformation.setTitle(("P" + (i + 1) + "-" + title.substring(8,title.length()-1)).replace(" ", ""));
                 videoInformation.setUrl(url + "?p=" + (i + 1));
                 videoInformations.add(videoInformation);
             }
